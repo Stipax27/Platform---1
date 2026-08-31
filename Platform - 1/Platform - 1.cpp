802e9dd97cd6195b6afca23b88cf76e4027afab3
gsadmin <gsadmin@DESKTOP-R7R9J7E>
2026-08-31 19:25:06 +0700

    Comment_code

--- Platform - 1/Platform - 1.cpp
+++ Platform - 1/Platform - 1.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
-
+// Данные игрока
 int pass;
 int wallet = 200;
-int payoff;
-int honesty;
 std::string answer;
 
-void ResultBribe() {
+// Данные охранника
+int payoff;
+int honesty;
+
+void ResultBribe() // Функция для определения исходов при взятке 
+{
 
 	
 	std::cout << "(Yes/No)\n";
 	std::cin >> answer;
 
-	if (answer == "Yes")
+	if (answer == "Yes")// Cогласие 
 	{
 
 		std::cout << "Give me the money\n";
-		if (wallet >= payoff)
+		if (wallet >= payoff) // Проверка на стоимость
 		{
 			std::cout << "Okay come on in\n";
 			wallet -= payoff;
@@ -32,11 +35,11 @@ void ResultBribe() {
 
 		}
 	}
-	else if (answer == "No")
+	else if (answer == "No") // Отказ 
 	{
 		std::cout << "If you don't want to give money, then leave.\n";
 	}
-	else
+	else // Невнятный ответ
 	{
 		std::cout << "What are you talking about.\n";
 	}
@@ -45,6 +48,7 @@ void ResultBribe() {
 int main()
 {
 	srand(time(0));
+	// присвоение и рандом данных
 	pass = rand() % 2;
 	payoff = 50 + rand() % 200;
 	honesty = 1 + rand() % 100;
@@ -52,7 +56,8 @@ int main()
 	std::cout << "My money: " << wallet << "\n";
 	std::cout << "Stalker, to get into the zone, provide a pass.\n";
 
-	if (pass == 0) {
+	if (pass == 0) // Проверка на наличие пропуска
+	{
 
 		std::cout << "I have a pass.\n";
 		std::cout << "You'r welcome!";
@@ -61,17 +66,17 @@ int main()
 	{
 		std::cout << "I don't have a pass.\n";
 
-		if (honesty <= 30)
+		if (honesty <= 30) // Проверка на честность меньше 30 
 		{
 			std::cout << "You can give me some money, and we'll settle the matter.\n";
 			ResultBribe();
 		}
-		else if (honesty >= 30 && honesty <=69) 
+		else if (honesty >= 30 && honesty <=69) // Проверка на честность от 30 до 69
 		{
 			std::cout << "I don't even know if he can give a bribe\n";
 			ResultBribe();
 		}
-		else
+		else // Проверка на честность от 70 до 100
 		{
 			std::cout << "I don't even know if he can give a bribe\n";
 			std::cout << "(Yes/No)\n";
