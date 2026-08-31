b69c61df591eb0cd328a13721c961893827f6e5b
gsadmin <gsadmin@DESKTOP-R7R9J7E>
2026-08-31 19:18:44 +0700

    save

--- Platform - 1/Platform - 1.cpp
+++ Platform - 1/Platform - 1.cpp
@@ -1,42 +1,94 @@
 #include <iostream>
 
+int pass;
+int wallet = 200;
+int payoff;
+int honesty;
+std::string answer;
+
+void ResultBribe() {
+
+	
+	std::cout << "(Yes/No)\n";
+	std::cin >> answer;
+
+	if (answer == "Yes")
+	{
+
+		std::cout << "Give me the money\n";
+		if (wallet >= payoff)
+		{
+			std::cout << "Okay come on in\n";
+			wallet -= payoff;
+			std::cout << "Player: I still have\n";
+			std::cout << wallet;
+
+		}
+		else
+		{
+
+			std::cout << "Player: Damn , I don't have enough money\n";
+			std::cout << "Yeah yeah get lost\n";
+
+		}
+	}
+	else if (answer == "No")
+	{
+		std::cout << "If you don't want to give money, then leave.\n";
+	}
+	else
+	{
+		std::cout << "What are you talking about.\n";
+	}
+}
+
 int main()
 {
-    // Данные атакующего
-    int strength = 100;
-    int weapon_damage = 50;
-    int сrit_chance = 15;
-    int crit_multiplier = 2;
+	srand(time(0));
+	pass = rand() % 2;
+	payoff = 50 + rand() % 200;
+	honesty = 1 + rand() % 100;
 
-    // Данные защищяющегося 
-    int barrier = 30;
-    int resistance = 20;
-    int dodge = 10;
-    int hp = 500;
+	std::cout << "My money: " << wallet << "\n";
+	std::cout << "Stalker, to get into the zone, provide a pass.\n";
 
-    // Вспомогательные переменные
-    int final_damage;
-    int final_resistance;
-    int crete_random = 1 + rand() % 100;
-    int dodge_random = 1 + rand() % 100;
+	if (pass == 0) {
 
-    //Логика данных и расчёты
-    final_damage = strength + weapon_damage;
+		std::cout << "I have a pass.\n";
+		std::cout << "You'r welcome!";
+	}
+	else 
+	{
+		std::cout << "I don't have a pass.\n";
 
-    if (crete_random <= сrit_chance) 
-    {
-        final_damage *= crit_multiplier;
-    }
+		if (honesty <= 30)
+		{
+			std::cout << "You can give me some money, and we'll settle the matter.\n";
+			ResultBribe();
+		}
+		else if (honesty >= 30 && honesty <=69) 
+		{
+			std::cout << "I don't even know if he can give a bribe\n";
+			ResultBribe();
+		}
+		else
+		{
+			std::cout << "I don't even know if he can give a bribe\n";
+			std::cout << "(Yes/No)\n";
+			std::cin >> answer;
 
-    final_resistance = ((final_damage/100)* resistance);
-    final_damage -= barrier + final_resistance;
-
-    if (dodge_random <= dodge)
-    {
-        final_damage = 0;
-    }
-
-    // Финальный расчёт и вывод
-    hp -= final_damage;
-    std::cout << hp;
+			if (answer == "Yes")
+			{
+				std::cout << "Leave right now, or I'll get a weapon.\n";
+			}
+			else if (answer == "No")
+			{
+				std::cout << "If you don't want to give money, then leave.\n";
+			}
+			else
+			{
+				std::cout << "What are you talking about.\n";
+			}
+		}
+	}
 }
